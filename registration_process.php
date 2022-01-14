86c3df83ab56d8540fb931cb129c0f14e835fd7a
kfc0105 <kentam633@gmail.com>
2022-01-13 20:48:30 -0800

    prevent duplicate account creation

--- registration_process.php
+++ registration_process.php
@@ -10,9 +10,9 @@
         echo 'Failed to connect to MySQL: '.$mysqli -> connect_error;
         exit();
     }else{
-        echo 'Database successfully connected!'.$mysqli -> connect_error;
-        echo '<br>';
-        echo '<br>';
+        //echo 'Database successfully connected!'.$mysqli -> connect_error;
+        //echo '<br>';
+        //echo '<br>';
     }
 
     $name = $_POST['name'];
@@ -20,7 +20,23 @@
     $password = $_POST['password'];
     $comment = '';
 
-    //Can add feature to validate the username and password for certaint requiremen
+    //Can add feature to validate the username and password for certaint requirement
+
+    //Verify for duplicate account
+    $sql = "SELECT * FROM accounts";
+    $result = $mysqli -> query($sql);
+    if($result -> num_rows > 0){
+        //Output each row of data
+        while($row = $result -> fetch_assoc()){
+            $uid_ref = $row["email"];
+            $uid_ref = strtolower($uid_ref);
+            if(strcmp($email, $uid_ref) == 0){
+                echo "The email already exists!<br><br>";
+                echo '<a href = "registration_process.php">Click here to Go back</a>';
+                exit();
+            }
+        }  
+    }
   
 
    // $sql = 'INSERT INTO Accounts(name, email, password) VALUES('.'"'.$name.'","'.$email.'","'.$password.'")';
